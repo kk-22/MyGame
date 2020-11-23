603c009160bfa6eea73adb15475c2d41c67c9ed6
dorann <dorannryuu@yahoo.co.jp>
2020-11-23 12:35:26 +0900

    Recyler viewを使って入力表作成

--- app/src/main/java/jp/co/my/mygame/SUBoxCell.kt
+++ app/src/main/java/jp/co/my/mygame/SUBoxCell.kt
@@ -1,14 +1,18 @@
 package jp.co.my.mygame
 
 import android.content.Context
-import android.util.AttributeSet
 import android.view.View
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class SUBoxCell(context: Context) : ConstraintLayout(context) {
 
     init {
         View.inflate(context, R.layout.su_box_cell, this)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        // 正方形にする
+        setMeasuredDimension(measuredWidth, measuredWidth)
     }
 }
