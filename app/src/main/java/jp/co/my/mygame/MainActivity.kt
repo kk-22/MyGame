20f9ade39a6182a4e7a6db077a0bacd119261349
kk-22 <kk22job@gmail.com>
2020-11-23 23:38:54 +0900

    Input number to cell

--- app/src/main/java/jp/co/my/mygame/MainActivity.kt
+++ app/src/main/java/jp/co/my/mygame/MainActivity.kt
@@ -2,10 +2,39 @@ package jp.co.my.mygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.su_box_cell.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputTable: SUInputTable
+    private lateinit var footerBar: SUFooterBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputTable = findViewById(R.id.input_table)
+        inputTable.cells.forEach {
+            it.forEach { cell ->
+                cell?.setOnClickListener(cellListener)
+            }
+        }
+        footerBar = findViewById(R.id.footer_bar)
+        footerBar.numberButtons.forEach { it.setOnClickListener(footerListener) }
+    }
+
+    private val cellListener = View.OnClickListener {
+        val numbers = footerBar.selectingNumbers()
+        if (numbers.count() == 0) {
+            return@OnClickListener
+        } else if (numbers.count() == 1) {
+            it.center_number_text.text = numbers[0]
+        } else {
+            it.center_number_text.text = numbers.joinToString(separator = "")
+        }
+    }
+
+    private val footerListener = View.OnClickListener {
     }
 }
