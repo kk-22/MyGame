ed00719e1cf40d5785305ed5ebbad0a69a0d46e1
kk-22 <kk22job@gmail.com>
2020-11-23 13:47:08 +0900

    Make table by for loop

--- app/src/main/java/jp/co/my/mygame/SUInputTable.kt
+++ app/src/main/java/jp/co/my/mygame/SUInputTable.kt
@@ -1,44 +1,90 @@
 package jp.co.my.mygame
 
 import android.content.Context
+import android.content.Context.WINDOW_SERVICE
 import android.util.AttributeSet
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.util.DisplayMetrics
+import android.view.WindowManager
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import kotlinx.android.synthetic.main.su_box_cell.view.*
 
 
-class SUInputTable(context: Context, attributeSet: AttributeSet) : RecyclerView(context, attributeSet) {
+class SUInputTable(context: Context, attributeSet: AttributeSet) : ConstraintLayout(context, attributeSet) {
+
+    private val cells = Array(9) { arrayOfNulls<SUBoxCell>(9) } // [列][行]
+    private val MAX_ROWS: Int = 9
 
     init {
-        val list = Array<String>(81) {"$it"}
-        adapter = SUCustomAdapter(list)
-        layoutManager = GridLayoutManager(context, 9, GridLayoutManager.VERTICAL, false)
-        setHasFixedSize(true)
-
-        addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-        addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL))
-    }
-}
-
-private class SUCustomAdapter(private val customList: Array<String>) :
-    RecyclerView.Adapter<SUCustomAdapter.SUCustomViewHolder>() {
-
-    class SUCustomViewHolder(val view: SUBoxCell) : RecyclerView.ViewHolder(view) {
+        createCells()
     }
 
-    override fun getItemCount(): Int {
-        return customList.size
-    }
+    private fun createCells() {
+        // 画面サイズ
+        val metrics = DisplayMetrics()
+        val windowManager = context.getSystemService(WINDOW_SERVICE) as WindowManager
+        windowManager.defaultDisplay.getMetrics(metrics)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SUCustomViewHolder {
-        val view = SUBoxCell(parent.context)
-        return SUCustomViewHolder(view)
-    }
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(this)
+        var topCell: SUBoxCell? = null // 1つ前の行の左端のセル
+        for (y in 0 until MAX_ROWS) { // 行
+            var leftCell: SUBoxCell? = null // 左のセル
+            for (x in 0 until MAX_ROWS) { // 列
+                val cell = SUBoxCell(context, x, y)
+                cell.id = 100 + y * 10 + x
+                cells[y][x] = cell
+                cell.center_number_text.text = cell.id.toString()
+                addView(cell)
 
-    override fun onBindViewHolder(holder: SUCustomViewHolder, position: Int) {
-        holder.view.center_number_text.text = customList[position]
+                constraintSet.setDimensionRatio(cell.id, "1:1")
+                constraintSet.constrainWidth(cell.id, metrics.widthPixels / 9)
+                constraintSet.constrainHeight(cell.id, 0)
+                leftCell?.let {
+                    // 2～9列目
+                    constraintSet.connect(cell.id, ConstraintSet.LEFT, it.id, ConstraintSet.RIGHT)
+                    constraintSet.connect(cell.id, ConstraintSet.TOP, it.id, ConstraintSet.TOP)
+                } ?: run {
+                    // 1列目
+                    constraintSet.connect(cell.id, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT )
+                    topCell?.let {
+                        constraintSet.connect(cell.id, ConstraintSet.TOP, it.id, ConstraintSet.BOTTOM)
+                        if (y == MAX_ROWS - 1) {
+                            // 最終行
+                            constraintSet.connect(cell.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
+                        }
+                    } ?: run {
+                        constraintSet.connect(cell.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
+                    }
+                }
+                if (x == 8) {
+                    // 最終列
+                    constraintSet.connect(cell.id, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT)
+                    topCell = cell
+                    leftCell = null
+                } else {
+                    leftCell = cell
+                }
+            }
+            constraintSet.createHorizontalChain(
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.RIGHT,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.LEFT,
+                cells[y].mapNotNull { it?.id }.toIntArray(),
+                null,
+                ConstraintSet.CHAIN_PACKED
+            )
+        }
+        constraintSet.createVerticalChain(
+            ConstraintSet.PARENT_ID,
+            ConstraintSet.TOP,
+            ConstraintSet.PARENT_ID,
+            ConstraintSet.BOTTOM,
+            cells.mapNotNull { it[0]?.id }.toIntArray(),
+            null,
+            ConstraintSet.CHAIN_PACKED
+        )
+        constraintSet.applyTo(this)
     }
 }
