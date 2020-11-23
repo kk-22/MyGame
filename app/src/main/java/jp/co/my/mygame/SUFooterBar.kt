7fa9363972ff02e1e4a4d9fae7d713e35ef883b6
dorann <dorannryuu@yahoo.co.jp>
2020-11-23 10:33:04 +0900

    Show table

--- app/src/main/java/jp/co/my/mygame/SUFooterBar.kt
+++ app/src/main/java/jp/co/my/mygame/SUFooterBar.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.ToggleButton
 import androidx.constraintlayout.widget.ConstraintLayout
 
