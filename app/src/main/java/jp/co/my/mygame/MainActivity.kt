dc3b35e9a4033061a9dae6772544f1b760bafd6f
dorann <dorannryuu@yahoo.co.jp>
2020-11-24 22:39:08 +0900

    Highlight cell that has same value of selected toggle button

--- app/src/main/java/jp/co/my/mygame/MainActivity.kt
+++ app/src/main/java/jp/co/my/mygame/MainActivity.kt
@@ -1,8 +1,8 @@
 package jp.co.my.mygame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.su_box_cell.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -27,14 +27,28 @@ class MainActivity : AppCompatActivity() {
     private val cellListener = View.OnClickListener {
         val cell = it as SUBoxCell
         val numbers = footerBar.selectingNumbers()
-
         when (numbers.count()) {
-            0 -> { return@OnClickListener }
+            0 -> { cell.center_number_text.text = "" }
             1 -> { cell.center_number_text.text = numbers[0] }
             else -> { cell.center_number_text.text = numbers.joinToString(separator = "") }
         }
     }
 
     private val footerListener = View.OnClickListener {
+        val selecting = footerBar.selectingNumbers()
+        when (selecting.count()) {
+            0, 2 -> {
+                // ハイライト解除
+                inputTable.updateAllStatus(SUStatus.NORMAL) { cell ->
+                    cell.status == SUStatus.SAME
+                }
+            }
+            1 -> {
+                // 数字が同じCellをハイライト
+                inputTable.updateAllStatus(SUStatus.SAME) { cell ->
+                    cell.center_number_text.text == selecting[0]
+                }
+            }
+        }
     }
 }
