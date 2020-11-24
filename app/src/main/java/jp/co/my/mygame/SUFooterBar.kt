ecb1743e188b7172739c52f7e346b2c687d11948
dorann <dorannryuu@yahoo.co.jp>
2020-11-24 21:44:32 +0900

    Highlight cell and delete no need ViewGroup

--- app/src/main/java/jp/co/my/mygame/SUFooterBar.kt
+++ app/src/main/java/jp/co/my/mygame/SUFooterBar.kt
@@ -7,14 +7,20 @@ import android.widget.Button
 import android.widget.ToggleButton
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class SUFooterBar(context : Context, attributeSet: AttributeSet) : ConstraintLayout(context, attributeSet) {
+
+class SUFooterBar(context: Context, attributeSet: AttributeSet) : ConstraintLayout(
+    context,
+    attributeSet
+) {
 
     val numberButtons: List<ToggleButton>
     init {
         View.inflate(context, R.layout.su_footer_bar, this)
 
-        val numberIds = arrayOf(R.id.button1, R.id.button2, R.id.button3, R.id.button4,
-            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9)
+        val numberIds = arrayOf(
+            R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9
+        )
         numberButtons = numberIds.map { findViewById<ToggleButton>(it) }
 
         findViewById<Button>(R.id.clear_button).setOnClickListener {
