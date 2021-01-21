d63cb5285c7b9146f5d2c426a36e7a343f3bc02b
dorann <dorannryuu@yahoo.co.jp>
2021-01-21 23:10:09 +0900

    Make MainActivity.kt

---
+++ app/src/main/java/jp/co/my/mygame/MainActivity.kt
@@ -0,0 +1,23 @@
+package jp.co.my.mygame
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.co.my.mygame.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sudokuButton.setOnClickListener {
+            val intent = Intent(applicationContext, SUPlayActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }
+    }
+}
