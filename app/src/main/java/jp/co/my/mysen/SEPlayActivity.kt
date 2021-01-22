f708d48196670fdc54ab3d37f4603123090e8d69
kk-22 <kk22job@gmail.com>
2021-01-22 21:27:22 +0900

    Make SEFieldView.kt

--- app/src/main/java/jp/co/my/mysen/SEPlayActivity.kt
+++ app/src/main/java/jp/co/my/mysen/SEPlayActivity.kt
@@ -6,10 +6,16 @@ import jp.co.my.mygame.databinding.SePlayActivityBinding
 
 class SEPlayActivity : AppCompatActivity() {
     private lateinit var binding: SePlayActivityBinding
+    private lateinit var balance: SEGameBalance
+    private lateinit var fieldView: SEFieldView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = SePlayActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        balance = SEGameBalance()
+        fieldView = binding.fieldView
+        fieldView.initialize(balance)
     }
 }
