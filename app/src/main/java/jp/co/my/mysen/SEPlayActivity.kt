d48e7142e180955005dd2a52e531e6883c75e12d
dorann <dorannryuu@yahoo.co.jp>
2021-01-22 23:42:37 +0900

    Draw land each the type

--- app/src/main/java/jp/co/my/mysen/SEPlayActivity.kt
+++ app/src/main/java/jp/co/my/mysen/SEPlayActivity.kt
@@ -3,6 +3,7 @@ package jp.co.my.mysen
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import jp.co.my.mygame.databinding.SePlayActivityBinding
+import jp.co.my.mysen.SELand.Type
 
 class SEPlayActivity : AppCompatActivity() {
     private lateinit var binding: SePlayActivityBinding
@@ -16,6 +17,17 @@ class SEPlayActivity : AppCompatActivity() {
 
         balance = SEGameBalance()
         fieldView = binding.fieldView
-        fieldView.initialize(balance)
+
+        val mockTypes = arrayOf(
+            Type.Grass, Type.Grass, Type.Fort, Type.Highway, Type.Grass,
+            Type.Grass, Type.Grass, Type.Grass, Type.Highway, Type.Grass,
+            Type.Grass, Type.Grass, Type.Grass, Type.Highway, Type.Grass,
+            Type.Grass, Type.Grass, Type.Highway, Type.Highway, Type.Grass,
+            Type.Grass, Type.Grass, Type.Fort, Type.Grass, Type.Grass,
+        )
+        val lands = mockTypes.mapIndexed { index, type ->
+            SELand(type, index % balance.fieldNumberOfX, index / balance.fieldNumberOfY)
+        }
+        fieldView.initialize(balance, lands)
     }
 }
