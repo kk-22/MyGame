22814b93f0e6e5aee33804dcdcdf33a6803ca1a7
dorann <dorannryuu@yahoo.co.jp>
2021-01-23 10:20:27 +0900

    Back to MainActivity by menu

--- app/src/main/java/jp/co/my/mysen/SEPlayActivity.kt
+++ app/src/main/java/jp/co/my/mysen/SEPlayActivity.kt
@@ -1,7 +1,11 @@
 package jp.co.my.mysen
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import jp.co.my.mygame.MainActivity
+import jp.co.my.mygame.R
 import jp.co.my.mygame.databinding.SePlayActivityBinding
 import jp.co.my.mysen.SELand.Type
 
@@ -29,5 +33,21 @@ class SEPlayActivity : AppCompatActivity() {
             SELand(type, index % balance.fieldNumberOfX, index / balance.fieldNumberOfY)
         }
         fieldView.initialize(balance, lands)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.se_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_back -> {
+                MainActivity.backToMain(this)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
