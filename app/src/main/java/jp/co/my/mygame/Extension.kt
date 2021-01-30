3398cd82b41afb485cb0593c05d877cbbbaf4688
dorann <dorannryuu@yahoo.co.jp>
2021-01-30 20:17:38 +0900

    Add extension that output log

--- app/src/main/java/jp/co/my/mygame/Extension.kt
+++ app/src/main/java/jp/co/my/mygame/Extension.kt
@@ -3,6 +3,8 @@ package jp.co.my.mygame
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.Log
+import android.widget.Toast
 import io.realm.RealmList
 
 fun Int.createBitmap(width: Int, height: Int, context: Context): Bitmap {
@@ -17,4 +19,13 @@ fun <T> RealmList<T>.deleteObject(obj: T) {
 
 fun <T> RealmList<T>.deleteAllObject(objs: List<T>) {
     objs.forEach { deleteObject(it) }
+}
+
+fun String.log() {
+    Log.d("tag", this)
+}
+
+fun String.toast(context: Context) {
+    log()
+    Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
 }
