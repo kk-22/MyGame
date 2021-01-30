3c5471df899dca0018f678e5eaafb0697a2a60e2
dorann <dorannryuu@yahoo.co.jp>
2021-01-30 19:41:05 +0900

    Delete unit after move from land

--- app/src/main/java/jp/co/my/mygame/Extension.kt
+++ app/src/main/java/jp/co/my/mygame/Extension.kt
@@ -3,8 +3,18 @@ package jp.co.my.mygame
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import io.realm.RealmList
 
 fun Int.createBitmap(width: Int, height: Int, context: Context): Bitmap {
     val baseImage = BitmapFactory.decodeResource(context.resources, this)
     return Bitmap.createScaledBitmap(baseImage, width, height, true)
+}
+
+fun <T> RealmList<T>.deleteObject(obj: T) {
+    val index = indexOf(obj)
+    deleteFromRealm(index)
+}
+
+fun <T> RealmList<T>.deleteAllObject(objs: List<T>) {
+    objs.forEach { deleteObject(it) }
 }
