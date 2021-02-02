776f38e1a7bca5233f91212e66577f29555e9d35
kk-22 <kk22job@gmail.com>
2021-02-02 23:55:26 +0900

    Make InfoView file

---
+++ app/src/main/java/jp/co/my/mysen/view/SEInfoStackView.kt
@@ -0,0 +1,9 @@
+package jp.co.my.mysen.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class SEInfoStackView(context: Context, attributeSet: AttributeSet) :
+    ConstraintLayout(context, attributeSet) {
+}
