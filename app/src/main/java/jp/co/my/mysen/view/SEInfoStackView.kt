30efdd4238c6927a7a3fabb4c629798071c1c65f
kk-22 <kk22job@gmail.com>
2021-05-05 13:25:40 +0900

    画面右上の情報表示機能を途中まで実装

--- app/src/main/java/jp/co/my/mysen/view/SEInfoStackView.kt
+++ app/src/main/java/jp/co/my/mysen/view/SEInfoStackView.kt
@@ -2,8 +2,17 @@ package jp.co.my.mysen.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import jp.co.my.mygame.databinding.SeViewInfoStackBinding
+import jp.co.my.mysen.realm.SELandRealmObject
 
 class SEInfoStackView(context: Context, attributeSet: AttributeSet) :
     ConstraintLayout(context, attributeSet) {
+
+    private val binding = SeViewInfoStackBinding.inflate(LayoutInflater.from(context), this, true)
+
+    fun updateLand(land: SELandRealmObject) {
+        binding.nameText.text = land.type.name
+    }
 }
